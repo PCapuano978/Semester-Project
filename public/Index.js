9689b467521a19d17372bb94953d40d8294a0a80
Unknown <pcapuano978@west-mec.org>
2019-12-18 23:43:45 -0700

    Third Major Commit
    Another day, more major progress to be had! Turns out, presenting Thursday instead, so I get to make this site look extra special. Will finish Yu-gi-oh stuff tomorrow morning.

--- public/Index.js
+++ public/Index.js
@@ -125,3 +125,199 @@ fetch (`https://pokeapi.co/api/v2/pokemon/${ID}/`)
             selector4.innerHTML = `<img src="${pokemon.back_default}" />`;
         }
 );
+
+/*
+* Digimon Start
+*/
+
+let digiID = 1;
+const testing = document.querySelector(".fa-arrow-alt-circle-right");
+console.log("should not be null, it SHOULD return a div",testing)
+var digiRight = document.querySelector(".fa-arrow-alt-circle-right").addEventListener("click", increaseDigi);
+var digiLeft = document.querySelector(".fa-arrow-alt-circle-left").addEventListener("click", decreaseDigi);
+
+// Key Event Listener
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+  
+    // Single Interval.
+    if (keyName === 'd') {
+        increaseDigi();
+    } else if(keyName === 'a') {
+        decreaseDigi();
+
+    // Interval by Ten.
+    } else if (keyName === 'w') {
+        increaseDigi(); increaseDigi(); increaseDigi(); increaseDigi(); increaseDigi();
+        increaseDigi(); increaseDigi(); increaseDigi(); increaseDigi(); increaseDigi();
+    } else if (keyName === 's') {
+        decreaseDigi(); decreaseDigi(); decreaseDigi(); decreaseDigi(); decreaseDigi(); 
+        decreaseDigi(); decreaseDigi(); decreaseDigi(); decreaseDigi(); decreaseDigi();  
+    }
+
+});
+
+
+function increaseDigi() {
+    digiID++;
+    if (digiID > 100) {
+        digiID = 1;
+     }
+
+    console.log(digiID);    
+    fetch (`https://digimon-api.herokuapp.com/api/digimon/id/${digiID}/`)
+    .then((res) => res.json())
+    .then( 
+        data => {
+            console.log("DATA:", data[0].name);
+            name = data.name
+            id = data.id
+            const [digimon] = data;
+            console.log(digimon)
+            const selector = document.querySelector(".digiIn");
+            selector.innerHTML = `<img src="${digimon.img}" />`;
+            const selector2 = document.querySelector(".digidex");
+            selector2.innerHTML = `${digimon.name}`;
+            const selector3 = document.querySelector(".numberD");
+            selector3.innerHTML = `Digidex #${digimon.id}`;
+
+        }
+);
+}
+
+function decreaseDigi() {
+    digiID--;
+    if (digiID < 1) {
+        digiID = 100;
+     } else if (digiID < 0) {
+        digiID = 1;
+    }
+
+    console.log(digiID);    
+    fetch (`https://digimon-api.herokuapp.com/api/digimon/id/${digiID}/`)
+    .then((res) => res.json())
+    .then( 
+        data => {
+            console.log("DATA:", data[0].name);
+            name = data.name
+            id = data.id
+            const [digimon] = data;
+            console.log(digimon)
+            const selector = document.querySelector(".digiIn");
+            selector.innerHTML = `<img src="${digimon.img}" />`;
+            const selector2 = document.querySelector(".digidex");
+            selector2.innerHTML = `${digimon.name}`;
+            const selector3 = document.querySelector(".numberD");
+            selector3.innerHTML = `Digidex #${digimon.id}`;
+
+        }
+);
+}
+
+fetch (`https://digimon-api.herokuapp.com/api/digimon/id/${digiID}/`)
+    .then((res) => res.json())
+    .then( 
+        data => {
+            console.log("DATA:", data[0].name);
+            name = data.name
+            id = data.id
+            const [digimon] = data;
+            console.log(digimon)
+            const selector = document.querySelector(".digiIn");
+            
+            selector.innerHTML = `<img src="${digimon.img}" />`;
+            const selector2 = document.querySelector(".digidex");
+            selector2.innerHTML = `${digimon.name}`;
+            const selector3 = document.querySelector(".numberD");
+            selector3.innerHTML = `Digidex #${digimon.id}`;
+
+        }
+);
+
+/*
+* Yu-gi-oh Start
+*/
+
+let yugiID = 1;
+
+var yugiRight = document.querySelector(".fa-arrow-alt-circle-right").addEventListener("click", increaseyugi);
+var yugiLeft = document.querySelector(".fa-arrow-alt-circle-left").addEventListener("click", decreaseyugi);
+
+// Key Event Listener
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+  
+    // Single Interval.
+    if (keyName === 'd') {
+        increaseyugi();
+    } else if(keyName === 'a') {
+        decreaseyugi();
+
+    // Interval by Ten.
+    } else if (keyName === 'w') {
+        increaseyugi(); increaseyugi(); increaseyugi(); increaseyugi(); increaseyugi();
+        increaseyugi(); increaseyugi(); increaseyugi(); increaseyugi(); increaseyugi();
+    } else if (keyName === 's') {
+        decreaseyugi(); decreaseyugi(); decreaseyugi(); decreaseyugi(); decreaseyugi(); 
+        decreaseyugi(); decreaseyugi(); decreaseyugi(); decreaseyugi(); decreaseyugi();  
+    }
+
+});
+
+
+function increaseyugi() {
+    yugiID++;
+    if (yugiID > 100) {
+        yugiID = 1;
+     }
+
+     fetch (`https://db.ygoprodeck.com/api/v5/cardinfo.php`)
+     .then((res) => res.json())
+     .then( 
+         data => {
+             console.log(data);
+             name = data.name
+             id = data.id
+             const selector = document.querySelector(".yugiImage");
+             selector.src = data[(0, yugiID)].card_images[0].image_url;
+ 
+         }
+ );
+}
+
+function decreaseyugi() {
+    yugiID--;
+    if (yugiID < 1) {
+        yugiID = 100;
+     } else if (yugiID < 0) {
+        yugiID = 1;
+    }
+
+    fetch (`https://db.ygoprodeck.com/api/v5/cardinfo.php`)
+    .then((res) => res.json())
+    .then( 
+        data => {
+            console.log(data);
+            name = data.name
+            id = data.id
+            const selector = document.querySelector(".yugiImage");
+            selector.src = data[(0, yugiID)].card_images[0].image_url;
+
+        }
+);
+}
+
+fetch (`https://db.ygoprodeck.com/api/v5/cardinfo.php`)
+    .then((res) => res.json())
+    .then( 
+        data => {
+            console.log(data);
+            name = data.name
+            id = data.id
+            const selector = document.querySelector(".yugiImage");
+            selector.src = data[(0, yugiID)].card_images[0].image_url;
+
+        }
+);
